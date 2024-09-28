d93296268ca291a5645f26b673efc13219ba1aa4
unknown <022a102@sxc.edu.np>
2024-09-28 11:46:42 +0545

    gonnaendit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,8 +16,6 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="(lib)" options={{ headerShown: false }} />
-      <Stack.Screen name="register" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
     </Stack>
   );
 }
